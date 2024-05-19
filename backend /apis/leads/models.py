53d7d803a254a6a1c3d73ec1e1bb740c17d2789e
andrey <polyakovv.andrey.01@gmail.com>
2024-05-19 23:04:06 +0300

    added analytics views 2

--- backend /apis/leads/models.py
+++ backend /apis/leads/models.py
@@ -12,6 +12,7 @@ class Lead(models.Model):
     action = models.CharField(max_length=1024)
     date = models.DateTimeField()
     interest_level = models.CharField(max_length=10, choices=INTEREST_LEVEL_CHOICES, default='medium')
+    revenue = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
     def __str__(self):
         return self.user
